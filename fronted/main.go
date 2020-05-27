e9a7b0fddb27e0c1a306adfc799c52da5199f91c
sebuntin <kirito331789567@gmail.com>
2020-05-27 12:52:33 +0800

    word correction

--- fronted/main.go
+++ fronted/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(View)))
-	http.Handle("/search", controller.CreatSearchHandler(TemplateFilePaht))
+	http.Handle("/search", controller.CreatSearchHandler(TemplateFilePath))
 	err := http.ListenAndServe(fmt.Sprintf(":%d", Fronted), nil)
 	if err != nil {
 		panic(err)
